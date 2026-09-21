ec3cae08f0db99d51e88e30fe90199c3bb4c9770
pokemonboy0505-dot <kyle@mercyhouseministry.com>
2026-09-21 15:12:55 -0700

    v1.90.0 - /apply command (paste actions, no AI), weekly sessions progress by period + deload version, replace by title, remove_rule by kind/title, founder grant back to 990 (sw v147)

---
+++ assets/index-DczEGopc.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-C5H3eeoe.js","./index-Z07Z2cWS.js","./index-XttmX87I.css"])))=>i.map(i=>d[i]);
+import{r,_ as p}from"./index-Z07Z2cWS.js";const o=r("App",{web:()=>p(()=>import("./web-C5H3eeoe.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.AppWeb)});export{o as App};
