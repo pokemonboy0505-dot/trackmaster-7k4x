370ddba62278dd321d4641ac7919a0461e43812b
pokemonboy0505-dot <kyle@mercyhouseministry.com>
2026-09-21 14:33:53 -0700

    v1.89.2 - coach re-asks for JSON when it answers in prose, zero-action change requests retried + flagged, client action cap 24 (sw v145)

---
+++ assets/index-BZWMQpbz.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-DhIg9PZ_.js","./index-T1kj5ajT.js","./index-XttmX87I.css"])))=>i.map(i=>d[i]);
+import{r,_ as p}from"./index-T1kj5ajT.js";const o=r("App",{web:()=>p(()=>import("./web-DhIg9PZ_.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.AppWeb)});export{o as App};
